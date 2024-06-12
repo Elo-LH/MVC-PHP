0004aa036c2395202d892312d83eea3db5196bf1
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-06-12 12:00:37 +0200

    league : match page

--- League Project/managers/MatchManager.php
+++ League Project/managers/MatchManager.php
@@ -71,7 +71,7 @@ class MatchManager extends AbstractManager
             return $team2['name'];
         }
     }
-    public function getwinner(int $id): ?string
+    public function getWinner(int $id): ?string
     {
         $query = $this->db->prepare('SELECT teams.name FROM teams JOIN games ON teams.id = games.winner WHERE games.id = :id LIMIT 1');
         $parameters = [
@@ -85,5 +85,23 @@ class MatchManager extends AbstractManager
         } else {
             return $winner['name'];
         }
+    }
+    public function getPerformances(int $id): array
+    {
+        $query = $this->db->prepare('SELECT * FROM games JOIN player_performance ON games.id = player_performance.game WHERE games.id = :id');
+        $parameters = [
+            'id' => $id,
+        ];
+        $query->execute($parameters);
+        $fetchedPerformances = $query->fetchAll(PDO::FETCH_ASSOC);
+        $performances = [];
+        //enter fetched performances from DB into instances array
+        foreach ($fetchedPerformances as $performance) {
+            $id = $performance['id'];
+            $performance = new Performance($performance['player'], $performance['game'], $performance['points'], $performance['assists']);
+            $performance->setId($id);
+            array_push($performances, $performance);
+        };
+        return $performances;
     }
 }
