88ea93b5fa12a7c46892d59615f358a354e79154
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-06-17 14:37:30 +0200

    secu blog : categories

--- Sécurité/Blog/managers/PostManager.php
+++ Sécurité/Blog/managers/PostManager.php
@@ -66,6 +66,7 @@ class PostManager extends AbstractManager
 
     public function findByCategory(int $categoryId): array
     {
+        $userManager = new UserManager;
         $query = $this->db->prepare('SELECT *, posts.title as post_title, posts.id as ost_id FROM posts_categories JOIN posts ON posts_categories.post_id = posts.id JOIN categories ON posts_categories.category_id = categories.id WHERE category_id = :id');
         $parameters = [
             'id' => $categoryId,
@@ -76,7 +77,8 @@ class PostManager extends AbstractManager
         //enter fetched users from DB into instances array
         foreach ($posts as $post) {
             $id = $post['post_id'];
-            $post = new Post($post['post_title'], $post['excerpt'], $post['content'], $post['author'], $post['created_at']);
+            $author = $userManager->findById($post['author']);
+            $post = new Post($post['post_title'], $post['excerpt'], $post['content'], $author, $post['created_at']);
             $post->setId($id);
             array_push($loadedPosts, $post);
         };
