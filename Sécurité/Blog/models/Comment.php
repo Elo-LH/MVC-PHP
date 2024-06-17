e4079d44e9e55e72f9011bdd5dc00c8d5045c592
Eloïse <131976796+Elo-LH@users.noreply.github.com>
2024-06-17 10:12:58 +0200

    secu mvc : models

--- Sécurité/Blog/models/Comment.php
+++ Sécurité/Blog/models/Comment.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author : Gaellan
  * @link : https://github.com/Gaellan
@@ -7,5 +8,48 @@
 
 class Comment
 {
+    private int $id = 0;
 
+    public function __construct(
+        private string $content,
+        private User $author,
+        private Post $post
+    ) {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+    public function setAuthor(User $author): void
+    {
+        $this->author = $author;
+    }
+
+    public function getPost(): Post
+    {
+        return $this->post;
+    }
+    public function setPost(Post $post): void
+    {
+        $this->post = $post;
+    }
 }
--- Sécurité/Blog/models/User.php
+++ Sécurité/Blog/models/User.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author : Gaellan
  * @link : https://github.com/Gaellan
@@ -7,5 +8,64 @@
 
 class User
 {
+    private ?int $id;
 
+    public function __construct(
+        private string $username,
+        private string $email,
+        private string $password,
+        private string $role,
+        private string $createdAt
+    ) {
+    }
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+    public function setRole(string $role): void
+    {
+        $this->role = $role;
+    }
+    public function getCreatedAt(): string
+    {
+        return $this->createdAt;
+    }
+    public function setCreatedAt(string $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
 }
